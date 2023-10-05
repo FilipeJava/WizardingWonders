c9c6c59e4333b771fcc34ce8a5d2c21540e5e0d4
mariananieton <rm94141@fiap.com.br>
2023-10-05 10:51:15 -0300

    Adicionando lógica + estilo

---
+++ Fiap.Web.WizardingWonders/TagHelpers/ConfirmTagHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Fiap.Web.WizardingWonders.TagHelpers;
+
+public class ConfirmTagHelper : TagHelper
+{
+    public string? Texto { get; set; }
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        output.TagName = "button";
+        output.Attributes.SetAttribute("class", "btn btn-secondary");
+        output.Content.SetContent(string.IsNullOrEmpty(Texto)?"Cadastrar":Texto);
+    }
+}
